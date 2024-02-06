e11d5632ff06f13c1941f47307dfaebb42058167
scallin <stephen.callin@callin.ca>
2024-02-06 18:52:21 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/comments', (req, res) => {
+  res.send('Comments page');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
