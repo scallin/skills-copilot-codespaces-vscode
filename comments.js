38d530be44e974b19188f7e09fac45a74224f0ef
scallin <stephen.callin@callin.ca>
2024-02-06 19:02:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,33 @@
 // create web server
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var comments = require('./comments.json');
 
-// create a route
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up the server to serve static files
+app.use(express.static('public'));
+
+// get request to /comments
+app.get('/comments', function(req, res) {
+  res.json(comments);
 });
 
-app.get('/comments', (req, res) => {
-  res.send('Comments page');
+// post request to /comments
+app.post('/comments', function(req, res) {
+  comments.push(req.body);
+  fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json(comments);
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// listen on port 3000
+app.listen(3000, function() {
+  console.log('Server started on port 3000');
 });
-
-// or don't
